2169cfb781ce05ac7bbfa8171f8ddb9ef4238fe5
andycanderson <chen.a.andy@gmail.com>
2014-10-01 16:10:05 -0700

    updated rspec version number

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,14 @@
 class Profile < ActiveRecord::Base
   belongs_to :applicant
   has_many :postings, through: :likes
+
+  def getTagArray
+    arr = []
+    tag_list = self.tag_list.downcase 
+    if tag_list.include? "c++"
+      arr << "c++"
+    end
+
+    arr
+  end
 end
