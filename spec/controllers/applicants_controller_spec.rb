ed9c945e035133d0cf3e7941d4fde228a40873e6
JpSchoch <jason.p.schoch@gmail.com>
2014-10-06 21:15:57 -0700

    rspec - 16 model specs and 9 controller specs

--- spec/controllers/applicants_controller_spec.rb
+++ spec/controllers/applicants_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ApplicantsController, :type => :controller do
 
-	it "renders applicant index" do
+	it "renders (get) applicant index" do
 		@applicants = assigns(:applicants) 
 		expect(response).to have_http_status(:success) 
 	end
