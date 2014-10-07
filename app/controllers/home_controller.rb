f76450051af6d2b1afcda524d4e0af4a4a0db67d
wdidan0101 <wdidan0101@gmail.com>
2014-10-06 20:21:42 -0700

    added about and contact page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,22 @@
 class HomeController < ApplicationController
+  layout "login"
   skip_before_action :is_logged_in?
   def index
     if @current_user
       sendMeHome(@current_user)
     end
+  end
+
+  def new 
+
+  end
+
+  def about
+
+  end
+
+  def contact
+
   end
 
    def sendMeHome user 
