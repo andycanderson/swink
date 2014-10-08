404cff930bd2ed793cb1720b6daf354726fa330b
andycanderson <chen.a.andy@gmail.com>
2014-10-08 12:30:51 -0700

    working on migrations

--- db/migrate/20141001012512_adding_relations_to_posting_tags_again.rb
+++
@@ -1,6 +0,0 @@
-class AddingRelationsToPostingTagsAgain < ActiveRecord::Migration
-  def change
-    add_column :posting_tags, :posting_id, :integer
-    add_column :posting_tags, :tag_id, :integer
-  end
-end
