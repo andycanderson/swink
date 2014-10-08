56580b7e80825582158a8dceff5e1ee9973f3752
andycanderson <chen.a.andy@gmail.com>
2014-10-07 17:45:17 -0700

    posting a job is transitioned

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,6 +3,7 @@ class LikesController < ApplicationController
 
 
   def likesave
+    # make like and add to stats on posting
     Like.create(posting_id: params[:id], profile_id: @current_user.profile.id, like: true, notify: true)
     post = Posting.find(params[:id])
     post.like += 1
@@ -12,6 +13,7 @@ class LikesController < ApplicationController
   end
 
   def dislikesave
+    # make like and add to stats on posting
     Like.create(posting_id: params[:id], profile_id: @current_user.profile.id, like: false)
     post = Posting.find(params[:id])
     post.dislike += 1
@@ -21,6 +23,7 @@ class LikesController < ApplicationController
   end
 
   def removenotification
+    # remove notification when recruiter sees profile
     like = Like.find(params[:id])
     like.update(notify: false)
     redirect_to seeprofile_path(like.profile_id)
