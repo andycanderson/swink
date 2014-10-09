b1f031f26f9dc45b33058e83a0e9c8e12580d0b3
wdidan0101 <wdidan0101@gmail.com>
2014-10-08 17:04:26 -0700

    old version, working on new draft now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
   def create
     # errors out because email not found short circuit authentication
     begin
-      user = User.find_by(email: params[:email].downcase)
+      user = User.find_by(email: params[:session][:email].downcase)
     rescue
       user = false
     end
 
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:session][:password])
       # sets user.id to a key in session
       session[:user_id] = user.id.to_s
       # brings user to their home page
